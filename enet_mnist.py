ebabbfbeb4d69d51f184e2507b24aa61facdabc4
gevero <giovi.pelle@gmail.com>
2018-04-16 23:12:40 +0200

    more steps

--- enet_mnist.py
+++ enet_mnist.py
@@ -32,7 +32,7 @@ def main(unused_argv):
         shuffle=True)
     mnist_classifier.train(
         input_fn=train_input_fn,
-        steps=2000,
+        steps=40000,
         hooks=[logging_hook])
 
     # Evaluate the model and print results
